7cefb2835c7bcbe6065ed9687eb1cfa70be1da14
alsami <al.khatib.sami1990@gmail.com>
2018-07-07 19:19:26 +0200

    add webapi sample

--- samples/MediatR.Extensions.Autofac.DepdencyInjection.Samples.WebApi/Startup.cs
+++ samples/MediatR.Extensions.Autofac.DepdencyInjection.Samples.WebApi/Startup.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 using Autofac;
 using MediatR.Extensions.Autofac.DepdencyInjection.Samples.WebApi.Filter;
 using MediatR.Extensions.Autofac.DependencyInjection;
+using MediatR.Extensions.Autofac.DependencyInjection.Samples.Shared.Commands;
 using MediatR.Extensions.Autofac.DependencyInjection.Samples.Shared.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace MediatR.Extensions.Autofac.DepdencyInjection.Samples.WebApi
 {
@@ -17,7 +20,13 @@ namespace MediatR.Extensions.Autofac.DepdencyInjection.Samples.WebApi
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.Filters.Add(typeof(CustomerNotFoundExceptionFilter)));
+            services.AddMvc(options => options.Filters.Add(typeof(CustomerNotFoundExceptionFilter)))
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
         }
 
         public void Configure(IApplicationBuilder app)
@@ -31,7 +40,7 @@ namespace MediatR.Extensions.Autofac.DepdencyInjection.Samples.WebApi
                 .As<ICustomersRepository>()
                 .SingleInstance();
             
-            builder.AddMediatR();
+            builder.AddMediatR(typeof(CustomerAddCommand).Assembly);
         }
     }
 }
