e34238ff886467aedd21aedf280dd327a5bc5229
alsami <al.khatib.sami1990@gmail.com>
2019-03-24 18:49:15 +0100

    setup travis build

---
+++ publish-packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd src
+for directory in *; do 
+    if [[ -d ${directory} ]]; then
+        cd ${directory}
+        dotnet pack *.csproj --include-symbols -c Release --output "."
+        dotnet nuget push -s ${1} -k ${2} "${directory}.${3}.symbols.nupkg"
+        if [[ ${?} != 0  ]]
+        then
+            exit -1
+        fi
+        cd ..
+    fi
+done
---
+++ run-tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+for d in test/*; 
+    do 
+        if [[ ${d} == *Tests ]]; then
+            cd ${d} 
+            dotnet test
+            if [[ ${?} != 0  ]]
+            then
+                exit -1
+            fi
+            cd ../../;
+        fi 
+done
